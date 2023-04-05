925c607561aa5e1ec3e0b81e3a363aa5b18f8b60
Arlen Manasov <arlen.manasov@icloud.com>
2023-04-05 17:31:41 +0600

    feat(home page): add playCta section

--- src/pages/home/sections/playCta/index.tsx
+++ src/pages/home/sections/playCta/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router'
 import { Box } from '@mui/material'
 
 import styles from './styles.module.scss'
 import shieldImg from './assets/images/shield.png'
+import ButtonCTA from 'shared/ui/ButtonCTA'
 
 const PlayCTA = () => {
+    const navigate = useNavigate()
+
     return (
-        <Box>
-            <img src={shieldImg} alt="" />
-            Let's Play!</Box>
+        <Box className={styles.playCtaWrapper}>
+            <div className={styles.imageWrapper}>
+                <img className={styles.shieldImg} src={shieldImg} alt="" />
+                <ButtonCTA className={styles.button} onClick={() => navigate('/game')}>Let's Play!</ButtonCTA>
+            </div>
+        </Box>
     )
 }
 
